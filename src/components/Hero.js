5f235cf3b3debf5f4e7db0ccdcb08ea67c299a52
Tammo <tammodewilde@gmail.com>
2023-06-19 13:34:37 +0200

    style updates howitworks, data-aos-offset fixed, etc.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,8 +19,8 @@ const Hero = () => {
       <div className="row">
         <div className="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">Stop searching for deals, let Whizzle do the work for you!</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">Save time and money with Whizzle's personalized sale alerts.</h2>
-          <div data-aos="fade-up" data-aos-delay="800">
+          <h2 data-aos="fade-up" data-aos-delay="200">Save time and money with Whizzle's personalized sale alerts.</h2>
+          <div data-aos="fade-up" data-aos-delay="400" data-aos-offset="80">
             <a href="#featuredbrands" className="btn-get-started scrollto">Get Started</a>
           </div>
         </div>
--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -12,24 +12,25 @@ const HowItWorks = () => {
     <section id="howitworks" className="services">
       <div className="container">
 
-        <div className="section-title" data-aos="fade-up">
+        <div className="section-title" data-aos="fade-up" data-aos-offset="60">
           <h2><span>How It Works</span></h2>
-          <p><strong>Never pay full price again -</strong> save money effortlessly with our customizable sale notifications. </p>
-          <p><strong> No more cluttered inbox -</strong> receive only the notifications you care about. </p>
+          <p><strong>Never pay full price again -</strong> save money effortlessly with our customizable sale notifications & never worry about a cluttered mailbox  </p>
         </div>
 
         <div className="howitworks-container">
 
-        <div className="howitworks-card" onClick={() => handleToggleDescription(0)}>            <div className="icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div className="icon"><i className="bx bxl-dribbble"></i></div>
+        <div className="howitworks-card" onClick={() => handleToggleDescription(0)}>            
+          <div className="icon-box" data-aos="fade-up" data-aos-delay="100" data-aos-offset="100">
+              <div className="icon"><i className="bi bi-check2-square"></i></div>
               <div className='howitworks-text'>
               <h4 className="title">Select brands</h4>                
               <p className={`description ${showDescriptionIndex === 0 ? 'show' : ''}`}>Select your favorite brands and webshops</p>              </div>
             </div>
           </div>
 
-          <div className="howitworks-card" onClick={() => handleToggleDescription(1)}>            <div className="icon-box" data-aos="fade-up" data-aos-delay="200">
-              <div className="icon"><i className="bx bx-file"></i></div>
+          <div className="howitworks-card" onClick={() => handleToggleDescription(1)}>            
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="200" data-aos-offset="80">
+              <div className="icon"><i className="bi bi-envelope-at"></i></div>
               <div className='howitworks-text'>
                 <h4 className="title">Provide us with your email</h4>
                 <p className={`description ${showDescriptionIndex === 1 ? 'show' : ''}`}>You are in control, selecte the desired frequency to create the best online shopping experience</p>
@@ -37,8 +38,9 @@ const HowItWorks = () => {
             </div>
           </div>
 
-          <div className="howitworks-card" onClick={() => handleToggleDescription(2)}>            <div className="icon-box" data-aos="fade-up" data-aos-delay="300">
-              <div className="icon"><i className="bx bx-tachometer"></i></div>
+          <div className="howitworks-card" onClick={() => handleToggleDescription(2)}>            
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="300" data-aos-offset="60">
+              <div className="icon"><i className="bi bi-search-heart"></i></div>
               <div className='howitworks-text'>
                 <h4 className="title">Automated Monitoring</h4>
                 <p className={`description ${showDescriptionIndex === 2 ? 'show' : ''}`}>Our automated systems will continuously monitor your webshops to detect sales</p>
@@ -46,8 +48,9 @@ const HowItWorks = () => {
             </div>
           </div>
 
-          <div className="howitworks-card" onClick={() => handleToggleDescription(3)}>            <div className="icon-box" data-aos="fade-up" data-aos-delay="400">
-              <div className="icon"><i className="bx bx-world"></i></div>
+          <div className="howitworks-card" onClick={() => handleToggleDescription(3)}>            
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="400" data-aos-offset="60">
+              <div className="icon"><i className="bi bi-chat-right-heart"></i></div>
               <div className='howitworks-text'>
                 <h4 className="title">Personalized Alerts</h4>
                 <p className={`description ${showDescriptionIndex === 3 ? 'show' : ''}`}>Receive personalized sale alerts, stress free.</p>
