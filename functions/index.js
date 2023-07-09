aaad3dbf9942d57b8b6d5dda6870c14f04c2a039
wisleythewise <jappievanleuven@gmail.com>
2023-07-09 19:55:40 +0200

    webhook handler

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,24 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const Stripe = require('stripe');
-const stripe = Stripe('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');
+const stripe = Stripe(functions.config().stripe.secret);
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 exports.createPaymentIntent = functions.https.onRequest((req, res) => {
   cors(req, res, async () => {
-    const { amount } = req.body;
+    const { amount, metadata } = req.body;
 
     try {
+
       const paymentIntent = await stripe.paymentIntents.create({
         amount,
-        currency: 'usd',
+        currency: 'eur', // iDEAL can only be used with Euros
+        payment_method_types: ['ideal'], // add this line
+        metadata : metadata
       });
+
 
       res.status(200).send(paymentIntent.client_secret);
     } catch (err) {
@@ -19,3 +26,34 @@ exports.createPaymentIntent = functions.https.onRequest((req, res) => {
     }
   });
 });
+
+
+exports.stripeWebhook = functions.https.onRequest(async (req, res) => {
+    let event;
+  
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        req.headers['stripe-signature'],
+        functions.config().stripe.webhook_secret
+      );
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+  
+    if (event.type === 'payment_intent.succeeded') {
+      const paymentIntent = event.data.object;
+      const { metadata } = paymentIntent;
+  
+      // Here you can update the user document in Firestore
+      const db = admin.firestore();
+      const userRef = db.collection('users').doc(metadata.customer_id);
+  
+      await userRef.update({
+        premiumUser: true
+      });
+    }
+  
+    res.json({received: true});
+  });
--- src/components/dashboard/Modals/PremiumModal.js
+++ src/components/dashboard/Modals/PremiumModal.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import { useNavigate } from "react-router-dom";
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { IdealBankElement } from '@stripe/react-stripe-js';
+import {UserContext} from '../../CTX/UserContext';
+
 
 const stripePromise = loadStripe('pk_live_51N98cKGPQQoxzNCweIkgGRhedoNd6L3GW2SZgKyDbPXcJSFknxocBV6VRDzAE6IeY9ITFR5BSLysxSPHi5SqU4up00xIc1uzsU');
 
@@ -12,10 +15,14 @@ const CheckoutForm = ({ clientSecret }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+    const { error, paymentIntent } = await stripe.confirmIdealPayment(clientSecret, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        ideal: elements.getElement(IdealBankElement),
+        billing_details: {
+          name: 'Discretion', // replace with your customer's name
+        },
       },
+      return_url: 'https://whizzle.ai', // replace with your return URL
     });
 
     if (error) {
@@ -26,16 +33,23 @@ const CheckoutForm = ({ clientSecret }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button type="submit" disabled={!stripe}>Pay</button>
+    <form className="checkout-form" onSubmit={handleSubmit}>
+      <IdealBankElement className="ideal-bank-element" />
+      <button className="checkout-button" type="submit" disabled={!stripe}>Pay</button>
     </form>
   );
 };
 
+
 const PremiumModal = ({ closeModal }) => {
   const navigate = useNavigate();
   const [clientSecret, setClientSecret] = React.useState(null);
+  const {currentUser, setCurrentUser} = useContext(UserContext);
+  const [show , setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true)
+  }, [currentUser])
 
   const handleUpgrade = () => {
     fetch('https://us-central1-whistle-58417.cloudfunctions.net/createPaymentIntent', {
@@ -44,7 +58,11 @@ const PremiumModal = ({ closeModal }) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        amount: 1000, // amount in cents
+        amount: 100, // amount in cents
+        metadata: {
+          customer_id: currentUser.uid, // replace with your customer's ID
+          // add more metadata fields as needed
+        },
       }),
     })
       .then(response => response.text())
@@ -52,20 +70,25 @@ const PremiumModal = ({ closeModal }) => {
       .catch(console.error);
   };
 
-  return (
-    <div className="modal-overlay" onClick={closeModal}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <h2>Premium Feature</h2>
-        <p>This is a premium feature. Please upgrade to access this feature.</p>
-        <button className='change-password' onClick={handleUpgrade}>Upgrade</button>
-        {clientSecret && (
-          <Elements stripe={stripePromise}>
-            <CheckoutForm clientSecret={clientSecret} />
-          </Elements>
-        )}
+  const uiModal = () => {
+    return (
+      <div className="modal-overlay" onClick={closeModal}>
+        <div className="modal-content" onClick={e => e.stopPropagation()}>
+          <h2>Premium Feature</h2>
+          <p>This is a premium feature. Please upgrade to access this feature.</p>
+          <button className='change-password' onClick={handleUpgrade}>Upgrade</button>
+          {clientSecret && (
+            <Elements stripe={stripePromise}>
+              <CheckoutForm clientSecret={clientSecret} />
+            </Elements>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
+
+  return (show ? uiModal() : <div>Loading...</div> )
 };
 
 export default PremiumModal;
